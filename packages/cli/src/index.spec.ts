6857f2bd3146c7bc7fafab679393d209990758a4
DonMahallem <donmahallem@users.noreply.github.com>
2021-07-09 21:22:23 +0200

    chore: save

---
+++ packages/cli/src/index.spec.ts
@@ -0,0 +1,13 @@
+/*!
+ * Source https://github.com/liesmich/liesmich Package: cli
+ */
+
+import { expect } from 'chai';
+import 'mocha';
+
+describe('geo-fence.ts', (): void => {
+    it('should resolve: ', (): void => {
+        // tslint:disable-next-line:no-unused-expression
+        expect('any', 'schema should not be valid').to.not.equal('cool');
+    });
+});
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,8 +1,5 @@
 /*!
- * Source https://github.com/liesmich/liesmich Package: schemas
+ * Source https://github.com/liesmich/liesmich Package: cli
  */
 
-export { GEO_FENCE_SCHEMA, IGeoFenceSchema } from './geo-fence';
-export { GET_VEHICLE_LOCATION_SCHEMA, IGeoLocationSchema } from './vehicle-location';
-export { ITripPassagesSchema, TRIP_PASSAGES_SCHEMA } from './trip-passages';
-export { IStopPassagesSchema, STOP_PASSAGES_SCHEMA } from './stop-passages';
+export const PLACEHOLDER: string = 'placeholder';
