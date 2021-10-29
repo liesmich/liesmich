95f0e92f94ace3e6d8c53d43f99186eda382190e
DonMahallem <donmahallem@users.noreply.github.com>
2021-10-29 21:15:14 +0200

    style(block): replace tslint with eslint

--- packages/block/src/block.ts
+++ packages/block/src/block.ts
@@ -3,27 +3,13 @@
  * Source https://liesmich.github.io/liesmich/
  */
 
-import { promises as fsp, Dirent } from 'fs';
 import { BlockEntry } from './block-entry';
 
 export class Block {
     private constructor(public readonly entries: BlockEntry) {}
 
-    public static async parseBlockEntry(path: Dirent): Promise<Block> {
-        const dirs: Dirent[] = await fsp.readdir(path.name, { withFileTypes: true });
-        for (const f of dirs) {
-            if (!f.isFile()) {
-                continue;
-            }
-        }
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        return new Block(dirs as any);
-    }
     public static async parseEntry(path: string): Promise<Block> {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        const block: Block = new Block(undefined as any);
-        const dirEntries: Dirent[] = await fsp.readdir(path, { withFileTypes: true });
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        return [block, dirEntries] as any;
+        const block: Block = new Block(await BlockEntry.fromFile(path));
+        return block;
     }
 }
