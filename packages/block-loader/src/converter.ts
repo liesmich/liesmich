a130cc9f4aaa346237aec6b607180a8cf51656be
DonMahallem <donmahallem@users.noreply.github.com>
2021-10-29 22:03:36 +0200

    style: fix lint

--- packages/block-loader/src/converter.ts
+++ packages/block-loader/src/converter.ts
@@ -7,5 +7,6 @@ import { parse, ParsedDocument } from '@liesmich/parser';
 
 export const loader = (data: string): string => {
     const doc: ParsedDocument = parse(data);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
     return JSON.stringify(doc);
 };
