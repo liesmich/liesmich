498b5b1a879e49ed60690bcd9e9745e6d6799697
DonMahallem <donmahallem@users.noreply.github.com>
2021-11-17 12:29:06 +0100

    fix(client): remove modify call

--- packages/client/server.ts
+++ packages/client/server.ts
@@ -36,7 +36,7 @@ export function app(): express.Express {
     }) as any;
     next();
   }
-  server.use(modify);
+  // server.use(modify);
   // All regular routes use the Universal engine
   server.get('*', (req, res) => {
     res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
