c98ded98c6392b3b0cfd906fbb9ca6601b343dac
DonMahallem <donmahallem@users.noreply.github.com>
2021-11-28 14:56:55 +0100

    test(client): setup tests

--- packages/client/src/app/app.component.spec.ts
+++ packages/client/src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
-import { Title } from '@angular/platform-browser';
+import { Component, DebugElement, Input } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By, Title } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { from, } from 'rxjs';
 import { AppComponent } from './app.component';
@@ -11,7 +11,7 @@ import { PostService } from './post.service';
   template: '<h1></h1>',
 })
 export class TestMarkdownComponent {
-  @Input('listmich-md')
+  @Input('liesmich-md')
   public data: any;
 }
 
@@ -29,35 +29,47 @@ describe('AppComponent', () => {
         {
           provide: Title,
           useValue: {
-
+            setTitle: (title: string): void => { }
           }
         },
         {
           provide: PostService,
           useValue: {
-            getData: () => from([]),
+            getData: () => from(['# markdown']),
           }
         }
       ]
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  describe('init', (): void => {
+    let fixture: ComponentFixture<AppComponent>;
+    let app: AppComponent;
+    beforeEach((): void => {
+      fixture = TestBed.createComponent(AppComponent);
+      app = fixture.componentInstance;
+      expect(app).toBeTruthy();
+    });
 
-  it(`should have as title 'liesmich'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('liesmich');
-  });
+    it(`should have as title 'liesmich'`, () => {
+      expect(app.title).toEqual('liesmich');
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).not.toBeDefined();
+    it('should render title', () => {
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('.content span')?.textContent).not.toBeDefined();
+    });
+    it('should read title', fakeAsync(() => {
+      fixture.detectChanges();
+      fixture.detectChanges();
+      fixture.detectChanges(); tick();
+      const childs: DebugElement[] = fixture.debugElement.queryAll(By.directive(TestMarkdownComponent));
+      expect(childs.length).toEqual(2);
+      const testMarkdown: string = "# yes\n i dont know\n 1. a\n2. b\n3. c\n\n| a | b | c | d |\n|---|---|---|---|\n| 1 | 2 | 3 | 4 |\n| 5 | 6 | 7 | 8 |\n\n" +
+        "`typescript asdf`\n\n```typescript\nconst a=2;\n```\n\n no";
+      expect((childs[0].componentInstance as TestMarkdownComponent).data).toEqual(testMarkdown);
+      expect((childs[1].componentInstance as TestMarkdownComponent).data).toEqual('# markdown');
+    }));
   });
 });
