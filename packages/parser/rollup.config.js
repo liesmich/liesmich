56b8cd063eb2661a13f25af561e4fb47750d28cc
DonMahallem <donmahallem@users.noreply.github.com>
2021-11-28 12:04:21 +0100

    chore(parser): update package lock

--- packages/parser/rollup.config.js
+++ packages/parser/rollup.config.js
@@ -1,4 +1,4 @@
 import defaultRollup from '@donmahallem/rollup-config';
 import pkg from './package.json';
 
-export default Object.assign(defaultRollup(pkg), { external: ["vfile", "spark-md5", "gray-matter"] });
+export default Object.assign(defaultRollup(pkg));
