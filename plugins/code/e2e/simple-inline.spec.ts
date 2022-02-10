f0e6b3a96f5d5da5aa9badd6d3870ca2bab97dd7
donmahallem <donmahallem@users.noreply.github.com>
2022-02-10 22:21:50 +0100

    feat(plugins-code): add plugin-code module

---
+++ plugins/code/e2e/simple-inline.spec.ts
@@ -0,0 +1,29 @@
+/*
+ * Package @liesmich/plugin-variable
+ * Source https://liesmich.github.io/liesmich/
+ */
+import { createPipeline } from '@liesmich-helpers/test-plugin';
+import { expect } from 'chai';
+import 'mocha';
+import { Processor } from 'unified';
+import { plugin } from './../src';
+
+// tslint:disable:no-unused-expression
+describe('e2e', (): void => {
+    let p: Processor;
+    let testData: Record<string, unknown>;
+    beforeEach((): void => {
+        testData = {};
+        p = createPipeline(plugin, testData);
+    });
+    describe('handle()', (): void => {
+        it('should not modify string', async (): Promise<void> => {
+            expect((await p.process('test')).toString()).to.equal('test\n');
+        });
+        it('should inline variable correclty', async (): Promise<void> => {
+            p.data('test', 'asdf');
+            expect((await p.process('test {{ lm:variable?key=test }} but no')).toString()).to.equal('test asdf but no\n');
+        });
+    });
+
+});
