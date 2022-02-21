2b6045719c42b1062ca24cb95856d1a6e0f8f66a
DonMahallem <donmahallem@users.noreply.github.com>
2022-02-21 14:55:27 +0100

    fix(converter): add handling off trailing empty space in lines

--- packages/converter/src/flow-tokenizer/tokenizer.ts
+++ packages/converter/src/flow-tokenizer/tokenizer.ts
@@ -5,26 +5,31 @@
 
 import { codes } from 'micromark-util-symbol/codes';
 import { Code, Construct, Effects, State } from 'micromark-util-types';
+import { Constants } from '../constants';
 import { liesmichConstructStart } from './../tokenizer/start-construct';
 
 export const liesmichConstructFlowStart: Construct = {
     name: 'liesmichFlowStart',
     tokenize: function liesmichTokenize(effects: Effects, ok: State, nok: State): State {
+        let spaceSuffix = 0;
         const start: State = (code: Code): State | void => {
             switch (code) {
                 case codes.space:
                 case codes.horizontalTab:
+                    if (spaceSuffix === 0) {
+                        effects.enter(Constants.LIESMICH_SPACE_SUFFIX);
+                    }
+                    spaceSuffix++;
                     effects.consume(code);
                     return start;
                 case codes.eof:
-                    return ok(code);
                 case codes.lineFeed:
                 case codes.carriageReturn:
                 case codes.carriageReturnLineFeed:
-                    effects.enter('lineEnding');
-                    effects.consume(code);
-                    effects.exit('lineEnding');
-                    return ok;
+                    if (spaceSuffix > 0) {
+                        effects.exit(Constants.LIESMICH_SPACE_SUFFIX);
+                    }
+                    return ok(code);
                 default:
                     return nok;
             }
