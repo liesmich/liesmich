5a69746dc0c51ecc8cb99f5ff8c09395ea54866d
DonMahallem <donmahallem@users.noreply.github.com>
2022-02-22 11:35:38 +0100

    chore: save

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -3,4 +3,21 @@
  * Source https://liesmich.github.io/liesmich/
  */
 
-export * from './fs';
+import { unified } from "unified";
+import { args } from "unified-args";
+
+args({
+    processor: unified,
+    name: 'liesmich',
+    description: 'liesmich readme generator',
+    version: '__BUILD_VERSION__',
+    pluginPrefix: 'remark',
+    extensions: ['remark-gfm',
+        'remark-parse',
+        'remark-stringify',
+        '@liesmich/converter'
+    ],
+    packageField: 'liesmich',
+    rcName: '.liesmichrc',
+    ignoreName: '.liesmichignore'
+})
