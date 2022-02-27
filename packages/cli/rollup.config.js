7f759f03a40bb0cd335d862bfdc2ffbc7729fccf
ò_ó <donmahallem@users.noreply.github.com>
2022-02-27 18:20:56 +0100

    feat(cli): use unified-args (#418)
    * chore: save
    
    * build(cli): fix bin package entry
    
    * style(cli): fix sort-key issues
    
    * build(cli): add shebang on bundle via rollup

--- packages/cli/rollup.config.js
+++ packages/cli/rollup.config.js
@@ -1,4 +1,17 @@
 import defaultRollup from '@donmahallem/rollup-config';
 import pkg from './package.json';
-
-export default Object.assign(defaultRollup(pkg));
+const copyFile = (options = {}) => {
+    return {
+        name: 'copy-file',
+        generateBundle(outputOptions, bundle, isWrite) {
+            for (const key of Object.keys(bundle)) {
+                if (bundle[key].isEntry === true) {
+                    bundle[key].code = '#!/usr/bin/env node\n' + bundle[key].code
+                }
+            }
+        },
+    }
+}
+const cfg = defaultRollup(pkg);
+cfg.plugins.push(copyFile());
+export default Object.assign(cfg);
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -3,4 +3,17 @@
  * Source https://liesmich.github.io/liesmich/
  */
 
-export * from './fs';
+import { unified } from 'unified';
+import { args } from 'unified-args';
+
+args({
+    description: 'liesmich readme generator',
+    extensions: ['remark-gfm', 'remark-parse', 'remark-stringify', '@liesmich/converter'],
+    ignoreName: '.liesmichignore',
+    name: 'liesmich',
+    packageField: 'liesmich',
+    pluginPrefix: 'remark',
+    processor: unified,
+    rcName: '.liesmichrc',
+    version: '__BUILD_VERSION__',
+});
