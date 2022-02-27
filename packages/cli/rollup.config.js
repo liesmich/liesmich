10cc4fb8072cb3bad8f7012f6bd704f1e6aff9a3
DonMahallem <donmahallem@users.noreply.github.com>
2022-02-27 19:36:43 +0100

    fix(cli): use @donmahallem/rollup-plugin-shebang

--- packages/cli/rollup.config.js
+++ packages/cli/rollup.config.js
@@ -1,17 +1,7 @@
 import defaultRollup from '@donmahallem/rollup-config';
+import shebang from '@donmahallem/rollup-plugin-shebang';
 import pkg from './package.json';
-const copyFile = (options = {}) => {
-    return {
-        name: 'copy-file',
-        generateBundle(outputOptions, bundle, isWrite) {
-            for (const key of Object.keys(bundle)) {
-                if (bundle[key].isEntry === true) {
-                    bundle[key].code = '#!/usr/bin/env node\n' + bundle[key].code
-                }
-            }
-        },
-    }
-}
+
 const cfg = defaultRollup(pkg);
-cfg.plugins.push(copyFile());
+cfg.plugins.push(shebang());
 export default Object.assign(cfg);
